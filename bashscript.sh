3973e6174294b8c86aaad0d573ac2342b011b48b
FerasMalkawi <feras.ghassan079@gmail.com>
2025-08-11 21:51:01 -0400

    Bashscript file has updated with new info

--- bashscript.sh
+++ bashscript.sh
@@ -1,1 +1,4 @@
 This file created By: Feras Malkawi
+
+What Bash Scripting ?
+* A plain text file that holds a series of commands to be run by the Bash shell is known as a Bash script. It serves as a program or automation tool that enables users to combine multiple commands and run them in a specified sequence.
